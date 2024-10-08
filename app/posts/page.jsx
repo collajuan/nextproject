58ff7891c73c68fc7112085980b6229fde084779
collajuan <collajuan@gmail.com>
2024-10-08 16:47:56 +0200

    PostPages

---
+++ app/posts/page.jsx
@@ -0,0 +1,19 @@
+async function loadPosts() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await res.json();
+  return data;
+}
+
+export default async function PostPages() {
+  const posts = await loadPosts();
+  return <div>
+    {
+        posts.map(post => (
+            <div key={post.id}>
+                <h3>{post.id} - {post.title}</h3>
+                <p>{post.body}</p>
+            </div>
+        ))
+    }
+  </div>;
+}
