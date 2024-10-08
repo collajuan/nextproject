90458499d4b029151226d652c2d036e63ad8cb7f
collajuan <collajuan@gmail.com>
2024-10-08 15:47:57 +0200

    navbar

--- app/layout.js
+++ app/layout.js
@@ -1,21 +1,11 @@
-import Link from 'next/link'
+
 
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
             <body>
                 <h1>Mi página</h1>
-                <nav>
-                    <ul>
-                        <li>
-                            <Link href='/'>Home</Link>
-                        </li>
-                        <li>
-                            <Link href='/about'>About</Link>
-                        </li>
-                        <li>Contact</li>
-                    </ul>
-                </nav>
+                
                 {children}
             </body>
         </html>
