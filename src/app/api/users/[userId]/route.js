512836292b8e707833fcbd4578f08829eb8d76d8
collajuan <collajuan@gmail.com>
2024-10-09 12:40:35 +0200

    searchParams Querys

---
+++ src/app/api/users/[userId]/route.js
@@ -0,0 +1,14 @@
+import { NextResponse } from 'next/server'
+
+export async function GET(request, {params}) {
+    const { searchParams } = new URL(request.url)
+    console.log(searchParams);
+    console.log(searchParams.get('apellido'));
+    
+    
+
+
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.userId}`)
+    const data = await res.json()
+    return NextResponse.json(data)
+}
